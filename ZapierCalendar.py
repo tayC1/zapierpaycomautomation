63b9e9f732be4762c1645486a5b30f84bc03e0a4
Taylor Giba <tay.christesson@gmail.com>
2025-08-27 12:13:41 -0500

    fixed python code

--- ZapierCalendar.py
+++ ZapierCalendar.py
@@ -2,11 +2,11 @@ import calendar
 from datetime import datetime, timedelta
 
 today = datetime.today()
-weekday = datetime.weekday() #monday = 0, Sunday = 6
+weekday = today.weekday() #monday = 0, Sunday = 6
 
 #find last day of month 
 last_day = calendar.monthrange(today.year, today.month)[1]
-last_day_date = calendar.datetime(today.year, today.month, last_day)
+last_day_date = datetime(today.year, today.month, last_day)
 last_day_weekday = last_day_date.weekday()
 
 #adjust if weekend 
@@ -18,16 +18,16 @@ else:
 
 #same for the fifteenth
 
-fifteenth = datetime(today.year, today.momth, 15)
+fifteenth = datetime(today.year, today.month, 15)
 fifteenth_weekday = fifteenth.weekday()
 
 if fifteenth_weekday >=5:
     offset = fifteenth_weekday -4 #will send back to Friday 
     fifteenth_workday = fifteenth-timedelta(days=offset)
 else:
-    ifteenth_weekday = fifteenth
+    fifteenth_workday = fifteenth
 
 #check if period matches either 
-is_last_day_of_period = today.date()==last_workday.date() or today.date() == fifteenth.workday.date()
+is_last_day_of_period = today.date()==last_workday.date() or today.date() == fifteenth_workday.date()
 
 output ={'run_zap': is_last_day_of_period} 
